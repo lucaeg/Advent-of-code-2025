6af8b37d20d5b0a34b591b651c6d7615558de3b9
lucaeg <lgaz@equinor.com>
2026-02-06 12:11:08 +0100

    clean day 3

--- day3.py
+++ day3.py
@@ -1,17 +1,16 @@
-file1 = open("day3.txt", "r")
-lines = file1.readlines()
+# read file and extract data
+with open("day3.txt", "r") as f:
+    lines = f.read().splitlines()
 
 res = 0
 for line in lines:
-    int_list = list(line)[:-1]
     res_str = ""
 
     for i in range(12):
-        j = 11 - i
-        index = len(int_list) - j
-        max_element = max(int_list[:index])
-        max_index = int_list.index(max_element)
-        int_list = int_list[(max_index + 1) :]
+        index = len(line) - (11 - i)
+        max_element = max(line[:index])
+        max_index = line.index(max_element)
+        line = line[(max_index + 1) :]
         res_str += max_element
 
     res += int(res_str)
